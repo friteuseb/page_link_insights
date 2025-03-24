e0f73745e9fe45ffdc6c4514fe7e194634c6ffbf
cyril.wolfangel@talan.com <cyril.wolfangel@talan.com>
2025-03-24 11:06:43 +0100

    update emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF['page_link_insights'] = [
     'clearCacheOnLoad' => true,
     'author' => 'Cyril Wolfangel',
     'author_email' => 'cyril.wolfangel@gmail.com',
-    'version' => '1.2.0',
+    'version' => '1.2.1',
     'constraints' => [
         'depends' => [
             'typo3' => '>=12.4.0,<14.0.0',
