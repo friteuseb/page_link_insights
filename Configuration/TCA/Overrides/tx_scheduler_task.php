7a9f2f0c6f0c1fcc2002fbe7497c217e8f95d798
Cyril <cyril.wolfangel@gmail.com>
2025-12-25 06:31:18 +0100

    fix: correct TCA registration for scheduler task in TYPO3 14

--- Configuration/TCA/Overrides/tx_scheduler_task.php
+++ Configuration/TCA/Overrides/tx_scheduler_task.php
@@ -7,29 +7,40 @@ use Cywolf\PageLinkInsights\Task\AnalyzeLinksTask;
 
 defined('TYPO3') or die();
 
-// Add custom columns for the scheduler task
-ExtensionManagementUtility::addTCAcolumns(
-    'tx_scheduler_task',
-    [
-        'rootPageId' => [
-            'label' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.field.rootPageId',
-            'config' => [
-                'type' => 'number',
-                'size' => 10,
-                'default' => 1,
-                'required' => true,
+if (isset($GLOBALS['TCA']['tx_scheduler_task'])) {
+    // Add custom columns for the scheduler task
+    ExtensionManagementUtility::addTCAcolumns(
+        'tx_scheduler_task',
+        [
+            'rootPageId' => [
+                'label' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.field.rootPageId',
+                'config' => [
+                    'type' => 'number',
+                    'size' => 10,
+                    'default' => 1,
+                    'required' => true,
+                ],
             ],
-        ],
-    ]
-);
+        ]
+    );
 
-// Register the task type with TCA
-ExtensionManagementUtility::addRecordType(
-    AnalyzeLinksTask::class,
-    [
-        'label' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.analyzeLinks.title',
-        'description' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.analyzeLinks.description',
-        'showitem' => '--div--;LLL:EXT:scheduler/Resources/Private/Language/locallang.xlf:tx_scheduler_task.basicConfiguration, rootPageId',
-    ],
-    'tx_scheduler_task'
-);
+    // Register the task type with TCA
+    ExtensionManagementUtility::addRecordType(
+        [
+            'label' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.analyzeLinks.title',
+            'description' => 'LLL:EXT:page_link_insights/Resources/Private/Language/locallang.xlf:task.analyzeLinks.description',
+            'value' => AnalyzeLinksTask::class,
+            'group' => 'page_link_insights',
+        ],
+        '
+            --div--;LLL:EXT:scheduler/Resources/Private/Language/locallang.xlf:scheduler.form.tabs.general,
+                tasktype,
+                rootPageId,
+            --div--;LLL:EXT:scheduler/Resources/Private/Language/locallang.xlf:scheduler.form.palettes.timing,
+                execution_details,
+        ',
+        [],
+        '',
+        'tx_scheduler_task'
+    );
+}
